3d28777f1b736fbe0a024b4563779d6d8da1a168
Jesse1224 <jessekartabani@gmail.com>
2022-12-08 12:31:15 +1100

    Add header animations

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,52 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function Header({}: Props) {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
         {/* Social media icons */}
         <SocialIcon
           fgColor="gray"
           bgColor="transparent"
           url="https://github.com/JesseKartabani"
         />
-      </div>
+      </motion.div>
 
-      <div className="flex flex-row items-center text-grey-300 cursor-pointer">
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-grey-300 cursor-pointer"
+      >
         {/* Contact Icon */}
         <SocialIcon
           className="cursor-pointer"
@@ -27,7 +58,7 @@ function Header({}: Props) {
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
           Get In Touch
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 }
