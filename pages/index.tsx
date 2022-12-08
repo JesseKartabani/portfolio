0b29a73d228cfc36d78562d21007ac4900ef4868
Jesse1224 <jessekartabani@gmail.com>
2022-12-09 01:46:12 +1100

    Change main scroll bar color & thumb color

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,8 @@ export default function Home() {
   return (
     <div
       className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
-     overflow-y-scroll overflow-x-hidden z-0"
+     overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20
+     scrollbar-thumb-[#F7AB0A]/80"
     >
       <Head>
         <title>Jesse Kartabani</title>
