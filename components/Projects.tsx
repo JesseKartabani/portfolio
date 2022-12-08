f9bce857a164a04cdd79c349d4792e7708520128
Jesse1224 <jessekartabani@gmail.com>
2022-12-09 01:49:55 +1100

    Change all scroll bars color & thumb color

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -19,7 +19,7 @@ function Projects({}: Props) {
 
       <div
         className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x
-      snap-mandatory z-20"
+      snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
       >
         {projects.map((project, i) => (
           <div
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -17,7 +17,10 @@ function WorkExperience({}: Props) {
         Experience
       </h3>
 
-      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+      <div
+        className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory
+      scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
+      >
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
