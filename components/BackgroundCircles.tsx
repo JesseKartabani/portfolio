845b3c1f7d7b5fa56bdb8e93f4b050f59c3c1393
Jesse1224 <jessekartabani@gmail.com>
2022-12-12 17:03:52 +1100

    Hide certain background circles on smaller screens

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -21,8 +21,8 @@ function BackgroundCircles({}: Props) {
       }}
       className="relative flex justify-center items-center"
     >
-      <div className="rounded-full  border border-[#333333] h-[200px] w-[200px] absolute mt-52 animate-ping" />
-      <div className="rounded-full  border border-[#333333] h-[300px] w-[300px] absolute mt-52" />
+      <div className="rounded-full hidden sm:block  border border-[#333333] h-[200px] w-[200px] absolute mt-52 animate-ping" />
+      <div className="rounded-full hidden sm:block  border border-[#333333] h-[300px] w-[300px] absolute mt-52" />
       <div className="rounded-full  border border-[#333333] h-[500px] w-[500px] absolute mt-52" />
       <div className="rounded-full  border border-[#F7AB0A] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse" />
       <div className="rounded-full  border border-[#333333] h-[800px] w-[800px] absolute mt-52" />
