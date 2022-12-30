a140b49d00e3c9e1b32046ad28d111d3fde80366
Jesse1224 <jessekartabani@gmail.com>
2022-12-30 23:04:54 +1100

    Make folder for constants and move project data

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,72 +2,11 @@ import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
+import { projects } from "../constants/projects.js";
 
 type Props = {};
 
 function Projects({}: Props) {
-  // Project data that we use to map over to display each project
-  const projects = [
-    {
-      id: 1,
-      name: "Clash Of Clans Stats",
-      description:
-        "Cross-platform mobile and web app for tracking and displaying statistics for the popular mobile game Clash of Clans. It has been built using React Native. The app uses its own backend Express server to call the Clash of Clans API and fetch data about players, clans, and other game information. It then uses the React Query library to manage and cache this data, providing optimized performance. The app displays the data in a user-friendly format on both mobile and web platforms",
-      website: "",
-      github: "https://github.com/JesseKartabani/clash-of-clans-stats",
-      image: "/cocProject.png",
-    },
-
-    {
-      id: 2,
-      name: "PokeDex",
-      description:
-        "Digital encyclopedia of Pokemon characters. It uses the PokeApi to fetch data about different Pokemon, including their statistics, and displays this information in an attractive and easy-to-use format with the help of animate.css. The project includes a search function to help users quickly find the Pokemon they are looking for. Built using React",
-      website: "https://pokedex-c348e.web.app/",
-      github: "https://github.com/JesseKartabani/Pokedex",
-      image: "/pokedexProject.png",
-    },
-
-    {
-      id: 3,
-      name: "Uber Clone",
-      description:
-        "This Uber clone uses several external APIs to provide its functionality, including the Google Places, Google Directions, and Google Distance Matrix APIs. These APIs allow the project to provide features such as searching for locations, tracking the progress of rides, and calculating estimated ride distances and times. In addition to these APIs, the project also uses the tailwind CSS framework and the Redux state management library. ",
-      website: "",
-      github: "https://github.com/JesseKartabani/Uber-clone",
-      image: "/uberProject.png",
-    },
-
-    {
-      id: 4,
-      name: "Netflix Clone",
-      description:
-        "Clone of the popular streaming service Netflix. It has been built using React and allows users to browse and watch movies and TV shows. The project uses TMDB API to fetch data about the available movies and shows.",
-      website: "https://netflix-clone-528b0.web.app/",
-      github: "https://github.com/JesseKartabani/netflix-clone",
-      image: "/netflixProject.png",
-    },
-
-    {
-      id: 5,
-      name: "NBA Stats",
-      description:
-        "Cross-platform mobile and web app for tracking and displaying NBA statistics. It has been built using React Native, and uses an external API to fetch data about NBA games and players.",
-      website: "https://nba-stats-3da74.web.app/",
-      github: "https://github.com/JesseKartabani/nba-stats",
-      image: "/nbaProject.png",
-    },
-
-    {
-      id: 6,
-      name: "Portfolio",
-      description: "",
-      website: "",
-      github: "https://github.com/JesseKartabani/portfolio",
-      image: "/portfolioProject.png",
-    },
-  ];
-
   return (
     <motion.div
       // Fades the whole component in
